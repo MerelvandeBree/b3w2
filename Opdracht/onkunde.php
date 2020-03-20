5dd3a769b311674b13ad57cd6ccaa11b069ac595
Merel <99059127@mydavinci.nl>
2020-03-20 14:31:59 +0100

    Php deel + css afgerond

--- Opdracht/onkunde.php
+++ Opdracht/onkunde.php
@@ -1,10 +1,94 @@
+<?php
+
+	$kunnen = "";
+	$opschieten = "";
+	$getal = "";
+	$vakantie = "";
+	$besteEigenschap = "";
+	$slechtsteEigenschap = "";
+	$overkomen = "";
+
+	if (isset($_POST["kunnen"]) && isset($_POST["opschieten"]) && isset($_POST["getal"]) && isset($_POST["vakantie"]) && isset($_POST["besteEigenschap"]) && isset($_POST["slechtsteEigenschap"]) && isset($_POST["overkomen"])) {
+	    $showForm = false;
+	}
+	else {
+		$showForm = true;
+	}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Mad libs - Onkunde</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
+
 <body>
-	<h1>Hi</h1>
+	<div id="container">
+	<h1>Mad libs</h1>
+		<ul>
+			<li><a href="paniek.php">Er heerst paniek...</a></li>
+			<li><a href="#">Onkunde</a></li>
+		</ul>
+		<div id="form">
+
+			<?php if ($showForm == true) { ?>
+
+			<h2>Onkunde</h2>
+
+			<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+
+				<label class="label" for="kunnen">Wat zou je graag willen kunnen?</label>
+				<input class="input" type="text" name="kunnen" value="<?echo $kunnen?>"><br><br>
+
+				<label class="label" for="opschieten">Met welk persoon kun je goed opschieten?</label>
+				<input class="input" type="text" name="opschieten" value="<?echo $opschieten?>"><br><br>
+
+				<label class="label" for="getal">Wat is je favoriete getal?</label>
+				<input class="input" type="text" name="getal" value="<?echo $getal?>"><br><br>
+
+				<label class="label" for="vakantie">Wat heb je altijd bij je als je op vakantie gaat?</label>
+				<input class="input" type="text" name="vakantie" value="<?echo $vakantie?>"><br><br>
+
+				<label class="label" for="besteEigenschap">Wat is je beste persoonlijke eigenschap?</label>
+				<input class="input" type="text" name="besteEigenschap" value="<?echo $besteEigenschap?>"><br><br>
+
+				<label class="label" for="slechtsteEigenschap">Wat is je slechtste persoonlijke eigenschap?</label>
+				<input class="input" type="text" name="slechtsteEigenschap" value="<?echo $slechtsteEigenschap?>"><br><br>
+
+				<label class="label" for="overkomen">Wat is het ergste wat je kan overkomen</label>
+				<input class="input" type="text" name="overkomen" value="<?echo $overkomen?>"><br><br>
+
+				<input id="button" type="submit" name="submit">
+
+				<div id="footer">
+					<span id="footer">© 2020 - Merel van de Bree</span>
+				</div>
+
+				<?php } ?>
+			</form>
+			
+		</div>
+	</div>
 
 </body>
 </html>
+
+<?php
+	if ($_SERVER["REQUEST_METHOD"] == "POST" && $showForm == false){
+?>
+
+	<div id='form'>
+
+	<h2>Onkunde</h2>
+
+	<p>Er zijn veel mensen die niet kunnen <?= $_POST["kunnen"] ?>. Neem nou <?= $_POST["opschieten"] ?> zelfs met de hulp van een <?= $_POST["vakantie"] ?> of zelfs <?= $_POST["getal"] ?> kan <?= $_POST["opschieten"] ?> niet <?= $_POST["kunnen"] ?>. Dat heeft niet temaken met een gebrek aan <?= $_POST["besteEigenschap"] ?>, maar met een te veel <?= $_POST["slechtsteEigenschap"] ?>. Te veel <?= $_POST["slechtsteEigenschap"] ?> leidt tot <?= $_POST["overkomen"] ?> en dat is niet goed als je wil <?= $_POST["kunnen"] ?>. Helaas voor <?= $_POST["opschieten"] ?>. </p>
+
+	<div id="footer">
+		<span id="footer">© 2020 - Merel van de Bree</span>
+	</div>
+
+	</div>
+	
+<?php } ?>
--- Opdracht/paniek.php
+++ Opdracht/paniek.php
@@ -1,9 +1,32 @@
-<!DOCTYPE html>
+<?php
+
+	$huisdier = "";
+	$persoon = "";
+	$land = "";
+	$vervelen = "";
+	$speelgoed = "";
+	$docent = "";
+	$kopen = "";
+	$bezigheid = "";
+
+	$showForm = true;
+
+	if (isset($_POST["huisdier"]) && isset($_POST["persoon"]) && isset($_POST["land"]) && isset($_POST["vervelen"]) && isset($_POST["speelgoed"]) && isset($_POST["docent"]) && isset($_POST["kopen"]) && isset($_POST["bezigheid"])) {
+	    $showForm = false;
+	}
+	else {
+		$showForm = true;
+	}
+
+?>
+
+<!DOCTYPE HMTL>
 <html>
 <head>
 	<title>Mad libs - Er heerst paniek</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
+
 <body>
 	<div id="container">
 	<h1>Mad libs</h1>
@@ -12,39 +35,81 @@
 			<li><a href="onkunde.php">Onkunde</a></li>
 		</ul>
 		<div id="form">
+
+			<?php if ($showForm == true) { ?>
+
 			<h2>Er heerst paniek...</h2>
+				
 			<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
 				<label class="label" for="huisdier">Welk dier zou je nooit als huisdier willen hebben?</label>
-				<input class="input" type="text" name="huisdier"><br><br>
+				<input class="input" type="text" name="huisdier" value="<?= $huisdier?>"><br><br>
 
 				<label class="label" for="persoon">Wie is de belangrijkste persoon in je leven?</label>
-				<input class="input" type="text" name="persoon"><br><br>
+				<input class="input" type="text" name="persoon" value="<?= $persoon?>"><br><br>
 
 				<label class="label" for="land">In welk land zou je graag willen wonen?</label>
-				<input class="input" type="text" name="land"><br><br>
+				<input class="input" type="text" name="land" value="<?= $land?>"><br><br>
 
 				<label class="label" for="vervelen">Wat doe je als je je verveelt?</label>
-				<input class="input" type="text" name="vervelen"><br><br>
+				<input class="input" type="text" name="vervelen" value="<?= $vervelen?>"><br><br>
 
 				<label class="label" for="speelgoed">Met welk speelgoed speelde je als kind het meest?</label>
-				<input class="input" type="text" name="speelgoed"><br><br>
+				<input class="input" type="text" name="speelgoed" value="<?= $speelgoed?>"><br><br>
 
-				<label class="label" for="spijbel">Bij welke docent spijbel je het liefst?</label>
-				<input class="input" type="text" name="spijbel"><br><br>
+				<label class="label" for="docent">Bij welke docent spijbel je het liefst?</label>
+				<input class="input" type="text" name="docent" value="<?= $docent?>"><br><br>
 
 				<label class="label" for="kopen">Als je &euro; 100.000,- had, wat zou je dan kopen?</label>
-				<input class="input" type="text" name="kopen"><br><br>
+				<input class="input" type="text" name="kopen" value="<?= $kopen?>"><br><br>
 
 				<label class="label" for="bezigheid">Wat is je favoriete bezigheid?</label>
-				<input class="input" type="text" name="bezigheid"><br><br>
+				<input class="input" type="text" name="bezigheid" value="<?= $bezigheid?>"><br><br>
 
 				<input id="button" type="submit" name="submit">
+
+				<div id="footer">
+					<span id="footer">© 2020 - Merel van de Bree</span>
+				</div>
+
+				<?php } ?>
 			</form>
-			<div id="footer">
-				<span id="footer">© 2020 - Merel van de Bree</span>
-			</div>
+			
 		</div>
 	</div>
 
 </body>
 </html>
+
+<?php
+	if ($_SERVER["REQUEST_METHOD"] == "POST" && $showForm == false){
+?>
+
+	<div id='form'>
+
+	<h2>Er heerst paniek...</h2>
+
+	<p>Er heerst paniek in het koningkrijk. <?= $_POST["land"] ?> Koning <?= $_POST["docent"] ?> is ten einde raad en als koning <?= $_POST["docent"] ?> ten einde raad is, dan roept hij zijn ten-einde-raadsheer <?= $_POST["persoon"] ?>.</p> 
+
+	<p>"<?= $_POST["persoon"] ?>! Het is een ramp! Het is een schande!"</p>
+
+	<p>"Sire, Majestijd, Uwe Luidruchtigheid, wat is er aan de hand?"</p>
+
+	<p>"Mijn <?= $_POST["huisdier"] ?> is verdwenen! Zomaar, zonder waarschuwing. En ik had net <?= $_POST["speelgoed"] ?> voor hem gekocht!"</p>
+
+	<p>"Majestijd, uw <?= $_POST["huisdier"] ?> komt vast weet terug?"</p>
+
+	<p>"Ja, da's leuk en aardig, maar hoe moet ik in de tussentijd <?= $_POST["bezigheid"] ?> leren?"</p>
+
+	<p>"Maar Sire, daar kunt u toch <?= $_POST["kopen"] ?> voor gebruiken?"</p>
+
+	<p>"<?= $_POST["persoon"] ?>, je hebt helemaal gelijk! Wat zou ik doen als ik jou niet had?"</p>
+
+	<p>"<?= $_POST["vervelen"] ?>, Sire."</p>
+
+	<div id="footer">
+		<span id="footer">© 2020 - Merel van de Bree</span>
+	</div>
+
+	</div>
+
+<?php } ?>
